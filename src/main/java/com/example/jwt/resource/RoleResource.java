6f80d5137f6dcce53facfbe16988c2bc3f0996d0
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-08-18 03:37:02 -0300

    First commit

---
+++ src/main/java/com/example/jwt/resource/RoleResource.java
@@ -0,0 +1,28 @@
+package com.example.jwt.resource;
+
+import com.example.jwt.model.RoleModel;
+import com.example.jwt.repository.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/roles")
+public class RoleResource {
+
+    private RoleRepository roleRepository;
+
+    @GetMapping
+    public List<RoleModel> findAll() {
+        return this.roleRepository.findAll();
+    }
+
+    @PostMapping
+    public RoleModel save(@RequestBody RoleModel roleModel) {
+        return this.roleRepository.save(roleModel);
+    }
+
+
+}
---
+++ src/main/java/com/example/jwt/service/UserService.java
@@ -0,0 +1,55 @@
+package com.example.jwt.service;
+
+import com.example.jwt.model.RoleModel;
+import com.example.jwt.model.UserModel;
+import com.example.jwt.repository.RoleRepository;
+import com.example.jwt.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserModel user = this.userRepository.findByEmail(email).orElseThrow();
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(RoleModel role: user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new User(user.getEmail(), user.getPassword(), authorities);
+    }
+
+    public UserModel save(UserModel userModel) {
+        userModel.setPassword(this.passwordEncoder.encode(userModel.getPassword()));
+        return this.userRepository.save(userModel);
+    }
+
+    public void addRoleToUser(String email, String name) {
+        UserModel user = this.userRepository.findByEmail(email).orElseThrow();
+        RoleModel role = this.roleRepository.findByName(name).orElseThrow();
+
+        user.getRoles().add(role);
+
+        this.userRepository.save(user);
+    }
+
+
+}
